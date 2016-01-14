aa82097c2f8ab5b3a9a74d28d2f07e96d64fa043
Alex Nguyen <alexnguyen@Alexs-MacBook-Pro.local>
2016-01-14 16:25:47 -0500

    Added post cell view controller, linked up everything into code. Currenty running with no compilation errors. Need to fix some styling issues as well as add more functionality

--- MyNeighbourhood/ViewController.swift
+++ MyNeighbourhood/ViewController.swift
@@ -6,20 +6,66 @@
 //  Copyright © 2016 Alex Nguyen. All rights reserved.
 //
 
+
+/*
+NOTES: 
+- Add UITableViewDelegate to access tableview stuff
+- Add UITableViewDataSource to access the data in table
+- Set tableView delegate/Data source to self
+- add numberOfSectionsInTableView function (usually return 1)
+- add tableView(....cellForRowAtIndexPath...) Get prototype cell
+- add tableView(....heightForRowAtIndexPath...) return height of cell
+- add tableView(....numberOfRowsInSection...) how many rows?
+
+*/
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var posts = [Post]()
+    
+    @IBOutlet weak var tableView: UITableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self //This class is the delegate
+        tableView.dataSource = self //Set where to get the data
+        
+        var post = Post(imagePath: "", title: "Post 1", description: "post 1 descp")
+        var post2 = Post(imagePath: "", title: "Post 2", description: "post 2 descp")
+        var post3 = Post(imagePath: "", title: "Post 3", description: "post 3 descp")
+        
+        posts.append(post)
+        posts.append(post2)
+        posts.append(post3)
+        
+        tableView.reloadData()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
     }
-
-
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let post = posts[indexPath.row] //Get third row, get third array 
+        
+        if let cell = tableView.dequeueReusableCellWithIdentifier("postCell") as? PostCell {
+            cell.configureCell(post)
+            return cell
+        } else {
+            let cell = PostCell()
+            cell.configureCell(post)
+            return cell
+        }
+        
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 86.5
+    }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
 }
 
---
+++ PostCell.swift
@@ -0,0 +1,29 @@
+//
+//  PostCell.swift
+//  MyNeighbourhood
+//
+//  Created by Alex Nguyen on 2016-01-14.
+//  Copyright © 2016 Alex Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var postImg: UIImageView!
+    
+    @IBOutlet weak var titleLbl: UILabel!
+    
+    @IBOutlet weak var descLbl: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    func configureCell(post: Post) {
+        titleLbl.text = post.title
+        descLbl.text = post.postDesc
+        
+    }
+}
