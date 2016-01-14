999d20a41ea9a3f786d27e9a0308e4e456746b5d
Alex Nguyen <alexnguyen@alexs-macbook-pro.local>
2016-01-14 13:48:06 -0500

    Initial Commit

---
+++ MyNeighbourhood/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyNeighbourhood
+//
+//  Created by Alex Nguyen on 2016-01-14.
+//  Copyright © 2016 Alex Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
